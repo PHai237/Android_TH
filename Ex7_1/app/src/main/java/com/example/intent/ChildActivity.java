a3362033fb1c7671a6799ffb2eb9ef3f992a6ea5
PHai237 <stone23072004@gmail.com>
2025-05-16 13:42:59 +0700

    Finished Ex7_1 Intent

--- Ex7_1/app/src/main/java/com/example/intent/ChildActivity.java
+++ Ex7_1/app/src/main/java/com/example/intent/ChildActivity.java
@@ -2,32 +2,23 @@ package com.example.intent;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class ChildActivity extends AppCompatActivity {
-    Button btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_child);
-        getSupportActionBar().setTitle("ChildActivity");
-        btnBack = findViewById(R.id.btnBack);
-        btnBack.setOnClickListener(v -> {
-            // Close ChildActivity
-            Intent intent = new Intent(ChildActivity.this, MainActivity.class);
-            startActivity(intent);
-        });
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Button button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChildActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
--- Ex7_2/app/src/main/java/com/example/solvelinearequation/MainActivity.java
+++ Ex7_2/app/src/main/java/com/example/solvelinearequation/MainActivity.java
@@ -1,9 +1,6 @@
-package com.example.intent;
+package com.example.solvelinearequation;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,14 +15,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ChildActivity.class);
-                startActivity(intent);
-            }
-        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
